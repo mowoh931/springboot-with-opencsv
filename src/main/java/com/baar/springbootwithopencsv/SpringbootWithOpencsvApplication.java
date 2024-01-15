84651b631e99b9d8889cb69f9f8a67d5b54ae585
JayMgitM <mowohbaar@yahoo.com>
2024-01-14 21:41:35 -0600

    Initial commit

---
+++ src/main/java/com/baar/springbootwithopencsv/SpringbootWithOpencsvApplication.java
@@ -0,0 +1,13 @@
+package com.baar.springbootwithopencsv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWithOpencsvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWithOpencsvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/baar/springbootwithopencsv/SpringbootWithOpencsvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baar.springbootwithopencsv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWithOpencsvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
